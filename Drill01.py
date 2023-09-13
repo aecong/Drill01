1b26d304db0ada7d59b13fa24a724ce9ed324a89
aecong <dkdus78655@gmail.com>
2023-09-13 23:26:30 +0900

    터틀 움직이고 stamp하기 추가

--- Drill01.py
+++ Drill01.py
@@ -1,9 +1,21 @@
 import turtle
 
 def left():
-
+    turtle.setheading(0)
+    turtle.left(180)
+    turtle.forward(50)
+    turtle.stamp()
 def right():
-
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
 def up():
-
+    turtle.setheading(0)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.stamp()
 def down():
+    turtle.setheading(0)
+    turtle.right(90)
+    turtle.stamp()
+
